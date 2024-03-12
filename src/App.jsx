a0726be21876f2f75e7971c605477279400e8cf9
LLetzel <lucasdiasletzel@gmail.com>
2024-03-12 14:44:57 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css';
+import Contato from './pages/Contato';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Contato />}/>
+          <Route path="/home" element={<Home />}/>
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,10 @@
+function Menu() {
+    return(
+        <div class="topicos">
+            <a href="/home"><strong>Home</strong></a>&nbsp;&nbsp;&nbsp;
+            <a href="/login"><strong>Login</strong></a> &nbsp;&nbsp;&nbsp;
+        </div>
+    )
+}
+
+export default Menu;
